de7aba0b347f06609f5100e281792fac6afd999e
Oh YoungJe <pine7420@naver.com>
2025-03-10 11:29:47 +0900

    [SUS-74] mock까지 완료

--- src/widgets/Member/SignupEmailVerificationGuide/ui/SignupEmailVerificationGuide.tsx
+++ src/widgets/Member/SignupEmailVerificationGuide/ui/SignupEmailVerificationGuide.tsx
@@ -1,16 +1,20 @@
 import { SignupEmailGuide } from '#entities/member'
-import { Standard } from '#shared/ui'
+import { StandardButton } from '#shared/ui'
 import { useLocation } from 'react-router-dom'
 import { sendSignupVerifyEmail } from '../model'
 import styles from './SignupEmailVerificationGuide.module.scss'
 
 export const SignupEmailVerificationGuide: React.FC = () => {
-  const email = new URLSearchParams(useLocation().search).get('email') || ''
+  const email = new URLSearchParams(useLocation().search).get('email')
+
+  if (!email) {
+    return null
+  }
 
   return (
     <div className={styles['email-verification-guide']}>
       <SignupEmailGuide email={email} />
-      <Standard
+      <StandardButton
         name={'인증 이메일 다시보내기'}
         onClick={() => {
           sendSignupVerifyEmail(email)
